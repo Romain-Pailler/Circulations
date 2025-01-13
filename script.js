8f6ad88153893e526f28f99ca32e6a43f974c433
Romain-Pailler <romain.pailler77@gmail.com>
2025-01-13 17:11:46 +0100

    retire le console.log

--- script.js
+++ script.js
@@ -128,7 +128,6 @@
       try {
           const weatherResponse = await axios.get(`https://www.infoclimat.fr/public-api/gfs/json?_ll=${lat},${long}&_auth=ARsDFFIsBCZRfFtsD3lSe1Q8ADUPeVRzBHgFZgtuAH1UMQNgUTNcPlU5VClSfVZkUn8AYVxmVW0Eb1I2WylSLgFgA25SNwRuUT1bPw83UnlUeAB9DzFUcwR4BWMLYwBhVCkDb1EzXCBVOFQoUmNWZlJnAH9cfFVsBGRSPVs1UjEBZwNkUjIEYVE6WyYPIFJjVGUAZg9mVD4EbwVhCzMAMFQzA2JRMlw5VThUKFJiVmtSZQBpXGtVbwRlUjVbKVIuARsDFFIsBCZRfFtsD3lSe1QyAD4PZA%3D%3D&_c=19f3aa7d766b6ba91191c8be71dd1ab2`);
           const weatherData = weatherResponse.data;
-          console.log(weatherData);
 
           const currentDate = new Date();
           const currentHour = currentDate.getHours();
